9a58d032000d5efaad0da268e6b26c82c85abde4
mengqingwen <mengqingwen@kuaishou.com>
2020-12-28 14:31:44 +0800

    fix

--- src/main/java/record/practice/linkedlist/Test.java
+++ src/main/java/record/practice/linkedlist/Test.java
@@ -9,6 +9,9 @@ import record.practice.base.ListNode;
 public class Test {
 
     public static void main(String[] args) {
+        char[] chars = "没人比我更懂Java".toCharArray();
+        System.out.println(chars.length);
+        System.out.println("没人比我更懂java".getBytes().length);
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(1);
         ListNode node3 = new ListNode(2);
@@ -65,5 +68,30 @@ public class Test {
             fast = fast.getNext().getNext();
         }
         return false;
+    }
+
+    /**
+     * 编写一个程序，找到两个单链表相交的起始节点。
+     *
+     * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+     * 输出：Reference of the node with value = 8
+     * 输入解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B
+     * 中，相交节点前有 3 个节点。
+     *
+     * 链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null){
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (!pA.getVal() .equals( pB.getVal())){
+            pA = pA.getNext() == null? headB:pA.getNext();
+            pB = pB.getNext() == null? headA:pB.getNext();
+        }
+        return pA;
     }
 }
