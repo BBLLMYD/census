e548cfcc4cc5671f87b2cea74f44908f371d2ab0
mengqingwen <mengqingwen@kuaishou.com>
2020-12-30 15:11:55 +0800

    fix

--- src/main/java/record/practice/linkedlist/Test.java
+++ src/main/java/record/practice/linkedlist/Test.java
@@ -15,18 +15,18 @@ public class Test {
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(2);
         ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(3);
-        ListNode node5 = new ListNode(2);
-        ListNode node6 = new ListNode(2);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(5);
         node1.setNext(node2);
         node2.setNext(node3);
         node3.setNext(node4);
         node4.setNext(node5);
         node5.setNext(node6);
-//        System.out.println(node1);
-//        ListNode node = removeElements(node1,3);
-//        System.out.println(node);
-        System.out.println(isPalindrome(node1));
+        System.out.println(node1);
+        ListNode node = deleteNode(node1,1);
+        System.out.println(node);
+//        System.out.println(isPalindrome(node1));
 
     }
 
@@ -38,8 +38,8 @@ public class Test {
      * @return
      */
     public static ListNode deleteDuplicates(ListNode head) {
-        if(head.getNext() == null){
-            return head;
+        if(head == null){
+            return null;
         }
         ListNode newHead = head;
         while(head.getNext()!= null){
@@ -176,5 +176,129 @@ public class Test {
             head = head.next;
         }
         return true;
+    }
+
+    /**
+     * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+     * 例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。这个链表的倒数第3
+     * 个节点是值为4的节点。
+     *
+     * 示例：
+     * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+     * 返回链表 4->5.
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public static ListNode getKthFromEnd(ListNode head, int k) {
+        if(head == null){
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        for(int i = 1 ; i <= k && fast!=null ; i++){
+            fast = fast.next;
+        }
+        while (fast!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+
+    /**
+     * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+     *
+     * 示例 1：
+     * 输入：head = [1,3,2]
+     * 输出：[2,3,1]
+     * @param head
+     * @return
+     */
+    public static int[] reversePrint(ListNode head) {
+        int cnt = 0;
+        ListNode cntHeader = head;
+        while(cntHeader!=null){
+            cntHeader = cntHeader.next;
+            cnt++;
+        }
+        int[] ints = new int[cnt];
+        for(int i=cnt-1; i>=0 ; i--){
+            ints[i] = head.val;
+            head = head.next;
+        }
+        return ints;
+    }
+
+    /**
+     * 链表中间节点
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode middleNode(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
+    /**
+     * 反转链表
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode next;
+        ListNode curr = head;
+        while (curr!=null){
+            next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return head;
+    }
+
+    /**
+     * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+     * 返回删除后的链表的头节点。
+     * 注意：此题对比原题有改动
+     * 示例 1:
+     * 输入: head = [4,5,1,9], val = 5
+     * 输出: [4,1,9]
+     * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public static ListNode deleteNode(ListNode head, int val) {
+        ListNode sNode = new ListNode(-1);
+        sNode.next = head;
+
+        ListNode pre = sNode;
+        ListNode curr = head;
+
+        while (curr != null){
+            if(curr.val == val){
+                pre.next = curr.next;
+            }else {
+                pre = curr;
+            }
+            curr = curr.next;
+        }
+        return sNode.next;
     }
 }
