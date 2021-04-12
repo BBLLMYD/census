1151e90874f38ea3d198dcdd69941937f81095b6
mengqingwen <mengqingwen@kuaishou.com>
2021-04-12 13:33:18 +0800

    ref-fix

---
+++ src/main/java/record/Main.java
@@ -0,0 +1,17 @@
+package record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengqingwen <mengqingwen@kuaishou.com>
+ * Created on 2021-04-12
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+
+}
