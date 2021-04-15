d6f3711fcade88c6be06d99fa14df8c186e68f71
BBLLMYD <mmzw0930@gmail.com>
2021-04-15 16:38:49 +0800

    ref-fix

---
+++ src/main/java/record/utils/ApplicationContextUtils.java
@@ -0,0 +1,25 @@
+package record.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author mengqingwen <mengqingwen@kuaishou.com>
+ * Created on 2021-03-09
+ */
+@Component
+public class ApplicationContextUtils implements ApplicationContextAware{
+
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return context;
+    }
+}
